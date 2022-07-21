6a5d4c0a4ce8e4116b1ada2247d808cc65f9a00c
hcweigand10 <henryweigand10@gmail.com>
2022-07-21 16:25:10 -0700

    updateUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -91,7 +91,21 @@ module.exports = {
     )
       .then((user) =>
         !user
-          ? res.status(404).json({ message: 'No user with this email!' })
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  updateUser(req,res) {
+    User.findOneAndUpdate(
+      { studentId: req.params.studentId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
           : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
