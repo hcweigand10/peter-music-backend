33bce6f40434c0a919bce5d2cbd8459dbf48ec8f
hcweigand10 <henryweigand10@gmail.com>
2022-08-16 21:09:04 -0700

    sessions

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
@@ -6,9 +6,17 @@ const { connect, connection } = require('mongoose');
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/peter-strasser-musicDB';
 
-connect(connectionString, {
+mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-module.exports = connection;
+mongoose.connection
+  .on("open", () => console.log("The goose is open"))
+  .on("close", () => console.log("The goose is closed"))
+  .on("error", (error) => {
+    console.log(error);
+    process.exit();
+  })
+
+module.exports = mongoose;
