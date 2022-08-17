fd9913596e64379d352239e8c4adf3d3d048d0fa
hcweigand10 <henryweigand10@gmail.com>
2022-08-16 20:01:42 -0700

    bye cors

--- server.js
+++ server.js
@@ -10,12 +10,25 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // // LOCAL
-app.use(cors());
+// app.use(cors());
 
 // DEPLOYED
 // app.use(cors({
 //   origin:"https://glittery-hotteok-47aca0.netlify.app/existing"
 // }))
+
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:3000', 'https://glittery-hotteok-47aca0.netlify.app/'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
