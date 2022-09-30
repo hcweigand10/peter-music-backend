68fde7f3821d34bda0b88e515668d9fd87f66712
hcweigand10 <henryweigand10@gmail.com>
2022-09-30 14:59:45 -0700

    getUsers

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,11 +44,8 @@ module.exports = {
   // Get all users
   getUsers(req, res) {
     User.find()
-      .then(async (users) => {
-        const userObj = {
-          users,
-        };
-        return res.json(userObj);
+      .then((users) => {
+        return res.json(users);
       })
       .catch((err) => {
         console.log(err);
